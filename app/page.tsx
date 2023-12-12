beeed07780cdff9b76585385e9d66d62bc154fc4
Gabo Esquivel <contact@gaboesquivel.com>
2023-12-12 14:15:20 -0600

    wip: simplify and display chat messages

--- app/page.tsx
+++ app/page.tsx
@@ -14,33 +14,37 @@ export default function App() {
   const [socket, setSocket] = useState<WebSocket>()
   const [messages, setMessages] = useState<Message[]>([])
 
-  const addMessage = (message:Message)=>{
-    //desconnecta del estado y solo lee el primer estado no lee el nuevo estado.
-    console.log('adding new message', message, messages.length)
-    setMessages([...messages, message, {"user":{"name":"Liam","color":"#FF8E76"},"body":"LUL haha"}, {"user":{"name":"Liam","color":"#FF8E76"},"body":"Lolololol"}])
-    // console.log(`Messages length ${messages.length}`)
-  }
-
   // connect to socket on load
   useEffect(() => {
     console.log('► connecting to socket server')
-    setSocket(connectToServer())
-  }, [])
+    const socket = connectToServer()
+    setSocket(socket)
+
+
+
+  }, [setSocket])
 
   // subscribe to message event and put them on state
-  useEffect(() => {
+  useEffect(()=>{
     if(!socket) return
-    socket.onmessage = (message) => {
-      addMessage(message.data)
-      // put it on back of execution queue to give time 
-      // to react to rerender before adding more messages
-      // setTimeout(()=>addMessage(message.data),0)
+    socket.onmessage = (message) =>  {
+      console.log('got message from socket', message.data)
+      setMessages([...messages, JSON.parse(message.data) as Message])
     }
-  }, [socket])
+  }, [messages, setMessages, socket] )
 
   return (
     <div>
       <p>Overmind Chat</p>
+      <ul>
+      {messages.map(({user, body}, index) => {
+        console.log({user, body})
+        return (<li key={index}>
+        <span className={`text-[${user.color.toString()}]`}>{user.name}</span> {body}
+      </li>)
+      })}
+      </ul>
+
     </div>
   );
 }
